b6880e87613b2aeef40e79e84d5595a86e58a9f0
18851972922 <56617340+18851972922@users.noreply.github.com>
2019-11-19 16:30:57 +0800

    Update hellloworld.java

--- src/test/hellloworld.java
+++ src/test/hellloworld.java
@@ -5,6 +5,7 @@ public class hellloworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("helloworld");
+		System.out.println("helloworld");
 	}
 
 }
